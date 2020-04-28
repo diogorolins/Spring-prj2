728a4445fd0a9ab5180fd5bd3ae08f72b0fae2ee
= <=>
2020-04-28 16:56:29 -0300

    Authorization

--- src/main/java/com/diogorolins/springprj1/config/SecurityConfig.java
+++ src/main/java/com/diogorolins/springprj1/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.diogorolins.springprj1.security.JWTAuthenticationFilter;
+import com.diogorolins.springprj1.security.JWTAuthorizationFilter;
 import com.diogorolins.springprj1.security.JWTUtil;
 
 @Configuration
@@ -39,7 +40,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_GET = { 
-			"/products/**", 
 			"/categories/**" ,
 			"/clients/**" 
 	};
@@ -56,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
