d6e3f702285f34607933b97463685772e06bbe62
= <=>
2020-05-18 09:10:53 -0300

    phones bug fix

--- src/main/java/com/diogorolins/springprj1/services/ProductService.java
+++ src/main/java/com/diogorolins/springprj1/services/ProductService.java
@@ -35,6 +35,7 @@ public class ProductService {
 	
 	public Page<Product> search(String name, List<Integer> ids, Integer page, 
 			Integer linesPerPage, String orderBy, String direction){
+		
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		List<Category> categories = categoryRepository.findAllById(ids);
 		Page<Product> result = repository.search(name, categories, pageRequest);
